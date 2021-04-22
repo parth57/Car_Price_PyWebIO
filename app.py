2497fc2c0949af6e464e099461d298f92005ba9d
Parth Agarwal <parthagarwal57@gmail.com>
2021-04-22 12:57:14 +0530

    First Commit

--- app.py
+++ app.py
@@ -53,15 +53,15 @@ app.add_url_rule('/tool', 'webio_view', webio_view(predict),
             methods=['GET', 'POST', 'OPTIONS'])
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
-    args = parser.parse_args()
+#if __name__ == '__main__':
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("-p", "--port", type=int, default=8080)
+    #args = parser.parse_args()
 
-    start_server(predict, port=args.port)
+    #start_server(predict, port=args.port)
 #if __name__ == '__main__':
     #predict()
 
-#app.run(host='localhost', port=80)
+app.run(host='localhost', port=80)
 
 #visit http://localhost/tool to open the PyWebIO application.
